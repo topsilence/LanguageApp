392c4fbc85137c7ecc41c90ad551bbef437a9c6f
Ryosuke Sakai <rkksakai@gmail.com>
2020-11-14 15:16:12 +0900

    delete file

--- common.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.js-menu__item__link').each(function(){
-        $(this).on('click',function(){
-            $("+.submenu",this).slideToggle();
-            return false;
-        });
-    });
-});
